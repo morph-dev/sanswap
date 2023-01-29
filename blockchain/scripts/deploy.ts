758d0537561de8d0d8e4fd7ea36dcaf75787c529
morph <82043364+morph-dev@users.noreply.github.com>
2023-01-29 21:51:08 +0200

    Fix deploy script

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -1,18 +1,34 @@
+import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+import { printWalletsBalances } from "./utils";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [owner, otherAccount] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const MintableToken = await ethers.getContractFactory("MintableToken");
+  const tokenA = await MintableToken.deploy("TokenA", "TA");
+  const tokenB = await MintableToken.deploy("TokenB", "TB");
+  const tokenC = await MintableToken.deploy("TokenC", "TC");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("TokenA deployed to:", tokenA.address);
+  console.log("TokenB deployed to:", tokenB.address);
+  console.log("TokenC deployed to:", tokenC.address);
 
-  await lock.deployed();
+  await printWalletsBalances(
+    [owner.address, otherAccount.address],
+    [tokenA, tokenB, tokenC]
+  );
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  await tokenA.transfer(otherAccount.address, parseEther("1000"));
+  await tokenB.transfer(otherAccount.address, parseEther("10000"));
+  await tokenC
+    .connect(otherAccount)
+    .mint(otherAccount.address, parseEther("100000"));
+
+  await printWalletsBalances(
+    [owner.address, otherAccount.address],
+    [tokenA, tokenB, tokenC]
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ blockchain/scripts/utils.ts
@@ -0,0 +1,59 @@
+import { BigNumber } from "ethers";
+import { formatUnits } from "ethers/lib/utils";
+import { MintableToken } from "../typechain-types";
+
+async function getWalletBalance(
+  wallet: string,
+  token: MintableToken
+): Promise<number> {
+  const decimals = await token.decimals();
+  return parseFloat(formatUnits(await token.balanceOf(wallet), decimals));
+}
+
+async function getWalletBalances(
+  wallet: string,
+  tokens: (readonly [string, MintableToken])[]
+): Promise<Record<string, number>> {
+  const balances = await Promise.all(
+    tokens.map(
+      async ([name, token]) =>
+        [name, await getWalletBalance(wallet, token)] as const
+    )
+  );
+  return Object.fromEntries(balances);
+}
+
+export async function printWalletBalances(
+  wallet: string,
+  tokens: MintableToken[]
+) {
+  const tokensWithNames = await Promise.all(
+    tokens.map(async (token) => [await token.name(), token] as const)
+  );
+  console.table(await getWalletBalances(wallet, tokensWithNames));
+}
+
+export async function printWalletsBalances(
+  wallets: string[],
+  tokens: MintableToken[]
+) {
+  const tokensWithNames = await Promise.all(
+    tokens.map(async (token) => [await token.name(), token] as const)
+  );
+
+  const balances = await Promise.all(
+    wallets.map(async (wallet) => {
+      const balances: Record<string, number | string> = await getWalletBalances(
+        wallet,
+        tokensWithNames
+      );
+      balances["address"] = wallet;
+      return balances;
+    })
+  );
+
+  console.table(balances, [
+    "address",
+    ...tokensWithNames.map(([name]) => name),
+  ]);
+}
