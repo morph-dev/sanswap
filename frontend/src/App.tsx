e3973ab7b817041ff14346921ec4e7f09ce2d4c8
morph <82043364+morph-dev@users.noreply.github.com>
2023-01-29 17:53:29 +0200

    Cleanup frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Box w="100%" p={4} gap={4} bg="green" color="white">
+      <Box fontSize="xl" fontWeight="bold">SanSwap</Box>
+      Work in progress, come back later.
+    </Box>
+  );
 }
 
-export default App
+export default App;
