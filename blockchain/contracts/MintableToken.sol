3944282eaac1a7d116e4b32e159302612cba0ff3
morph <82043364+morph-dev@users.noreply.github.com>
2023-01-29 18:28:37 +0200

    Create sample SmartContract and configure hardhat.config.ts

---
+++ blockchain/contracts/MintableToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MintableToken is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_URL,
+      accounts: {
+        mnemonic: process.env.WALLET_MNEMONIC,
+        count: 10,
+      }
+    }
+  }
 };
 
 export default config;
